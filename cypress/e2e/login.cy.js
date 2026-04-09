92abba4204ee442e4f20758640bc1f3c5cfe2de0
begalve-lopes <begalve.927390438.00@gmail.com>
2026-04-09 15:09:32 +0100

    O Dev estragou minha massa. E agora?

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,25 @@ describe("Login", () => {
       name: "Begas Silva",
       email: "begas@email.com",
       password: "123456",
+      is_provider: true,
     };
+
+    before(() => {
+      cy.task("deleteUser", user.email).then((resultado) => {
+        console.log(resultado);
+      });
+
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3333/users",
+        body: user,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("name", user.name);
+        expect(response.body).to.have.property("email", user.email);
+      });
+    });
+
     it("Deve logar com sucesso", () => {
       loginPage.go();
       loginPage.form(user);
